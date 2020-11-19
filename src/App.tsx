55c75042e7c8b991dfc90b17f253aa0848ee1923
Zachary Graham <zakgraham@gmail.com>
2020-11-18 20:05:48 -0700

    create header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+
+//components
+import Header from "./component/Header/Header";
+import Footer from "./component/Footer/Footer";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Footer />
       zg-photography
+      <Header/>
     </div>
   );
 }
---
+++ src/component/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <div>Footer</div>
+    )
+}
+
+export default Footer;
